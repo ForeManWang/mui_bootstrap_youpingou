4e42a2990d4f0f1d8d83017bf0230c73b854fd01
ForeManWang <formanW@aliyun.com>
2019-01-18 18:32:00 +0800

    项目接口文档

--- docs/letao初始化.sql
+++ docs/letao初始化.sql
@@ -1,8 +1,9 @@
 /*
 SQLyog Ultimate v12.09 (64 bit)
-MySQL - 5.5.20-log : Database - letao
+MySQL - 5.5.20-log : Database - youpingou
 *********************************************************************
-*/
+*/
+
 
 /*!40101 SET NAMES utf8 */;
 
@@ -12,9 +13,9 @@ MySQL - 5.5.20-log : Database - letao
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`letao` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`youpingou` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
-USE `letao`;
+USE `youpingou`;
 
 /*Table structure for table `address` */
 
