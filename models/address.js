da668a74272917ddcaf516a74660887a95632eaf
ForeManWang <formanW@aliyun.com>
2019-01-18 18:32:53 +0800

    项目后台文件

--- models/address.js
+++ models/address.js
@@ -12,7 +12,7 @@ function Address(address) {
 
 Address.addAddress = function (address, callback) {
   var selectSql = 'insert into address (id,userId,address,addressDetail,isDelete,recipients,postcode)  values (null,?,?,?,1,?,?)';
-  db.query(selectSql, [address.userId, address.address, address.addressDetail,address.recipients,address.postcode], function (err, result) {
+  db.query(selectSql, [address.userId, address.address, address.addressDetail, address.recipients, address.postcode], function (err, result) {
     console.log(err);
     if (err) {
       return callback(err);
@@ -22,7 +22,7 @@ Address.addAddress = function (address, callback) {
 };
 Address.updateAddress = function (address, callback) {
   var selectSql = 'UPDATE address SET  ';
-    var param = new Array();
+  var param = new Array();
   if (address.address && param.length == 0) {
     selectSql += ' address=? ';
     param[param.length] = address.address;
@@ -30,30 +30,27 @@ Address.updateAddress = function (address, callback) {
   if (address.addressDetail && param.length == 0) {
     selectSql += ' addressDetail=? ';
     param[param.length] = address.addressDetail;
-  }
-  else if (address.addressDetail && param.length != 0) {
+  } else if (address.addressDetail && param.length != 0) {
     selectSql += ' ,addressDetail=? ';
     param[param.length] = address.addressDetail;
   }
-   if (address.recipients && param.length == 0) {
+  if (address.recipients && param.length == 0) {
     selectSql += ' recipients=? ';
     param[param.length] = address.recipients;
-  }
-  else if (address.recipients && param.length != 0) {
+  } else if (address.recipients && param.length != 0) {
     selectSql += ' ,recipients=? ';
     param[param.length] = address.recipients;
   }
-     if (address.postcode && param.length == 0) {
+  if (address.postcode && param.length == 0) {
     selectSql += ' postcode=? ';
     param[param.length] = address.postcode;
-  }
-  else if (address.postcode && param.length != 0) {
+  } else if (address.postcode && param.length != 0) {
     selectSql += ' ,postcode=? ';
     param[param.length] = address.postcode;
   }
-     selectSql += ' WHERE id=? ';
-   param[param.length] = address.id;
-  db.query(selectSql,param, function (err, res) {
+  selectSql += ' WHERE id=? ';
+  param[param.length] = address.id;
+  db.query(selectSql, param, function (err, res) {
     if (err) {
       return callback(err);
     }
@@ -62,7 +59,7 @@ Address.updateAddress = function (address, callback) {
 }
 Address.deleteAddress = function (id, callback) {
   var delSql = "UPDATE address SET isDelete =0 WHERE id =?";
-  db.query(delSql,[id],function (err, res) {
+  db.query(delSql, [id], function (err, res) {
     if (err) {
       return callback(err);
     }
